373b12e3408ac7f3e34c0b1938237f99b289c219
gregorywpower <gregorywpower@gmail.com>
2020-08-26 00:45:03 -0400

    remove depricated scripts

--- DASHNextGen_Master.py
+++ DASHNextGen_Master.py
@@ -1,12 +1,7 @@
 print("DASHNextGen_Master.py is starting")
 import DASHNextGen_Builder
-# import DASHNextGen_File
 import DASHNextGen_File_delimited
-# import DASHNextGen_job
 import DASHNextGen_job_delimited
-# import DASHNextGen_Service (Deprecated in favor of DASHNextGen_Service_Report)
-# import DASHNextGen_Service_delimited (Deprecated in favor of DASHNextGen_Service_Report)
-# import DASHNextGen_Service_occurred (Deprecated in favor of DASHNextGen_Service_Report)
 import DASHNextGen_Service_Report
 import beeper_module
 print("DASHNextGen_Master.py is done")
