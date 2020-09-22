17cf0a66dd6472fedf698d98970202ece4fb5647
gregorywpower <gregorywpower@gmail.com>
2020-09-22 16:14:52 -0400

    add project page clicker

--- DASH_Utility_Scripts/project_model_clicker.py
+++ DASH_Utility_Scripts/project_model_clicker.py
@@ -1,6 +1,86 @@
-""""
-#TODO:
-1. Need to select every project.
-2. Select All Items in Dropdown.
-3. Click Save
-""""
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.webdriver import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+import pandas as pd
+from datetime import datetime
+import time
+
+"""
+This was the original method I was using when developing this script, please run this if you are curious of what is happening under the hood of Selenium or you need to troubleshoot any issues.
+"""
+# print("Real Browser Launching")
+# browser = webdriver.Chrome(ChromeDriverManager().install())
+# print("Real Browser has Launched")
+
+"""
+The Headless browsing option greatly reduces the amount of time it takes for the scraper to run.
+"""
+print("Headless Browser Running")
+options = Options()
+options.add_argument("--headless") # Runs Chrome in headless mode.
+options.add_argument('--no-sandbox') # Bypass OS security model
+options.add_argument('--disable-gpu')  # applicable to windows os only
+options.add_argument('start-maximized') # 
+options.add_argument('disable-infobars')
+options.add_argument("--disable-extensions")
+browser = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+print("Headless Browser has Launched")
+
+def login_into_dash(json_target_file):
+    """
+    Takes the login information from JSON file and passes data to login form.
+
+    Parameter json_target_file needs to be equal to the file's location.
+
+    Contents of the file must be organized as follows [Note: don't forget the curly braces]:
+    
+    {
+    "username": "please-put-your-username-here",
+    "password": "please-put-your-password-here"
+    }
+
+
+    """
+    browser.get("http://sem.myirate.com/")
+    with open(json_target_file) as login_data:
+        data = json.load(login_data)
+    username = data['username']
+    password = data['password']
+    browser.find_element_by_name("ctl00$ContentPlaceHolder1$Username").send_keys(username)
+    browser.find_element_by_name("ctl00$ContentPlaceHolder1$Password").send_keys(password)
+    browser.find_element_by_name("ctl00$ContentPlaceHolder1$btnLogin").click()
+
+def click_all_items_submit(url):
+    browser.get(url)
+    browser.find_element_by_id("ctl00_ContentPlaceHolder1_Add_Model_Arrow").click()
+    plans_boxes = browser.find_elements_by_class_name("rcbCheckBox")
+    
+    for items in range(0, len(plans_boxes)):
+        try:
+            plans_boxes[items].click()
+        except:
+            WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.ID, "rcbCheckBox"))).click()
+    browser.find_element_by_name("ctl00$ContentPlaceHolder1$btnAdd").click()
+
+
+
+def logout_session():
+    browser.get("http://sem.myirate.com/Dashboard_Company.aspx")
+    browser.find_element_by_xpath('//*[@id="navProfile"]').click()
+    try:
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.LINK_TEXT,"Log Out"))).click()
+    except:
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.LINK_TEXT,"Log Out"))).click()
+
+def main():
+    login_into_dash("./DASHLoginInfo.json")
+    click_all_items_submit("http://sem.myirate.com/Projects/Project_Edit_Model.aspx?id=2383")
+    logout_session()
+
+main()
